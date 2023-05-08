46b1a326abe17f725c1c9e9a4e79af80afc58fba
Kiana <114351440+Kianaquasary@users.noreply.github.com>
2023-05-08 07:25:38 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,49 +1,32 @@
-import flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-
-app = flask.Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/test_db7'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///steps.db'
 db = SQLAlchemy(app)
 
-
-class Message(db.Model):
+class Steps(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(512), nullable=False)
+    steps = db.Column(db.Integer)
+    date = db.Column(db.String(50))
 
-    def __init__(self, text, tags):
-        self.text = text
-        self.tags = [
-            Tag(text=tag) for tag in tags.split(',')
-        ]
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        steps = request.form['steps']
+        date = request.form['date']
+        step_data = Steps(steps=steps, date=date)
+        db.session.add(step_data)
+        db.session.commit()
+        return redirect(url_for('home'))
+    all_steps = Steps.query.all()
+    return render_template('index.html', steps=all_steps)
 
-
-class Tag(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(32), nullable=False)
-
-    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
-    message = db.relationship('Message', backref=db.backref('tags', lazy=True))
-
-
-@app.route('/', methods=['GET'])
-def hello():
-    return flask.render_template('index.html', messages=Message.query.all())
-
-
-@app.route('/add_message', methods=['POST'])
-def add_message():
-    text = flask.request.form['text']
-    tag = flask.request.form['tag']
-    # messages.append(Message(text, tag))
-    db.session.add(Message(text, tag))
+@app.route('/clear')
+def clear():
+    db.session.query(Steps).delete()
     db.session.commit()
+    return redirect(url_for('home'))
 
-    return flask.redirect(flask.url_for('hello'))
-
-
-
-
-with app.app_context():
-    db.create_all()
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
